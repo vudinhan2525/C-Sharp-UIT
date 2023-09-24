a93880c064a6d3d244c385cc48fbf3a117a1596d
vudinhan2525 <vudinhan000@gmail.com>
2023-09-24 20:39:41 +0700

    nt

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+namespace newApp
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        public struct MyPoint{
+            public int x,y;
+            public MyPoint (int p1,int p2){
+                x = p1;
+                y = p2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyPoint a = new MyPoint(1,2);
+            Console.WriteLine("{0},{1}",a.x,a.y);
+        }
+    }
+}
