670049ed5076b99a34535e5fb8bb8f83c765c601
vudinhan2525 <vudinhan000@gmail.com>
2023-10-10 18:16:23 +0700

    nt

---
+++ Baithuchanh2/Bai1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualBasic;
+namespace Baithuchanh2_1
+{   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao duong dan thu muc: ");
+            string strFile = Console.ReadLine();
+            DirectoryInfo src1 = new DirectoryInfo(strFile);
+            if(src1.Exists){
+                DirectoryInfo[] dirs = src1.GetDirectories();
+                FileInfo[] files = src1.GetFiles();
+                foreach(DirectoryInfo dir in dirs){
+                    Console.WriteLine(dir.CreationTime + "\t\t <DIR> \t\t" + dir.Name);
+                }
+                foreach(FileInfo file in files){
+                    Console.WriteLine(file.CreationTime + "\t\t "  + file.Length + " \t\t" + file.Name);
+                }
+            }
+            else{
+                Console.WriteLine("Not have this path");
+            }
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualBasic;
+namespace test
+{   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao duong dan thu muc: ");
+            string strFile = Console.ReadLine();
+            DirectoryInfo src1 = new DirectoryInfo(strFile);
+            if(src1.Exists){
+                FileInfo[] dirs = src1.GetFiles();
+                foreach(FileInfo dir in dirs){
+                    Console.WriteLine(dir.Name);
+                }
+            }
+            else{
+                Console.WriteLine("Not have this path");
+            }
+        }
+    }
+}
